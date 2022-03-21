1b9fb4d9541071c3da0e2f4a3eb7d9fc2207c79b
stevari <villevartia@hotmail.com>
2022-03-21 12:27:11 +0200

    osa1 lisätty. sisältää tehtävät 1.2 asti

---
+++ osa1/kurssitiedot/newapp/src/App.js
@@ -0,0 +1,61 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14     
+
+
+  return (
+    <>
+      
+      <Header course = {course} />
+      <Content p1 = {part1} e1 = {exercises1} p2 = {part2} e2 = {exercises2} p3 = {part3} e3 = {exercises3}/>
+      <Total e1 ={exercises1} e2={exercises2} e3 ={exercises3}/>
+
+    </>
+  )
+
+}
+const Part = (props) => {
+  const partname = props.partname
+  const exercises = props.exercises
+  return (
+    <div>
+      {partname} {exercises}
+    </div>
+  )
+}
+const Content = (props) => {
+  
+  return (
+    <div>
+     <Part partname = {props.p1} exercises = {props.e1}/>
+     <Part partname = {props.p2} exercises = {props.e2}/>
+     <Part partname = {props.p3} exercises = {props.e3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  
+  const exercises = props.e1+props.e2+props.e3
+  return (
+    <div>
+      <p>Number of exercises {exercises}</p>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+
+
+export default App
