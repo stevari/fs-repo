92c2dc21f81907e0446cae847f34040905343120
stevari <villevartia@hotmail.com>
2022-03-27 18:58:59 +0300

    osa 1.6

--- osa1/kurssitiedot/newapp/src/App.js
+++ osa1/kurssitiedot/newapp/src/App.js
@@ -1,4 +1,5 @@
 const App = () => {
+  
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -31,6 +32,7 @@ const App = () => {
        <Part name = {course.parts[0].name} exercises ={course.parts[0].exercises}/>
        <Part name = {course.parts[1].name} exercises ={course.parts[1].exercises}/>
        <Part name = {course.parts[2].name} exercises ={course.parts[2].exercises}/>
+       
        
       
       </div>
