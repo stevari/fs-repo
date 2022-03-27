503e02b127a213bc24a0ff849babede49a924f10
stevari <villevartia@hotmail.com>
2022-03-27 19:49:55 +0300

    unicafe step1

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -1,15 +1,50 @@
 import { useState } from 'react'
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+const Display = ({ value }) => <div>{value}</div>
 
 const App = () => {
   // tallenna napit omaan tilaansa
+  
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  const addGoodRev = () => setGood(good + 1)
+  const addNeutralRev = () => setNeutral(neutral + 1)
+  const addBadRev = () => setBad(bad + 1)
 
   return (
     <div>
-      code here
+      <h2>Give feedback!</h2>
+      <br></br>
+      <Button
+        handleClick={addGoodRev}
+        text='good'
+      />
+      <Button
+        handleClick={addNeutralRev}
+        text='neutral'
+      />
+      <Button
+        handleClick={addBadRev}
+        text='bad'
+      />
+      <br></br>
+
+      <h2>Statistics:</h2> 
+
+      <br></br> 
+      <Display value={good}/>
+      <Display value={neutral}/>
+      <Display value={bad}/>
+         
     </div>
+      
+  
   )
 }
 
