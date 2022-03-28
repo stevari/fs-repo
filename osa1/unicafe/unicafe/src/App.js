dcb20d0c881bc13b7173a28f9c65896bc1723d91
stevari <villevartia@hotmail.com>
2022-03-28 18:14:18 +0300

    t 1.7, unicafe step2

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -5,6 +5,29 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 const Display = ({ value }) => <div>{value}</div>
+const StatisticLine = ({ text,value,text2 }) => <div>{text} {value} {text2} </div>
+const Statistics = ({good,neutral,bad}) => {
+  /// ...
+  let all = good+neutral+bad;
+  let average = 0;
+  average = (good -1*bad)/all;
+
+  let pospercentige = 0;
+  pospercentige = (good/all)*100;
+  return(
+    
+    <div>
+      <StatisticLine text="good" value ={good} />
+      <StatisticLine text="neutral" value ={neutral} />
+      <StatisticLine text="bad" value ={bad} />
+      <StatisticLine text="all" value ={all} />
+      <StatisticLine text="average" value ={average} />
+      <StatisticLine text="positive" value ={pospercentige} text2="%" />
+      
+
+    </div>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
@@ -12,10 +35,12 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
   
   const addGoodRev = () => setGood(good + 1)
   const addNeutralRev = () => setNeutral(neutral + 1)
   const addBadRev = () => setBad(bad + 1)
+ 
 
   return (
     <div>
@@ -38,9 +63,10 @@ const App = () => {
       <h2>Statistics:</h2> 
 
       <br></br> 
-      <Display value={good}/>
-      <Display value={neutral}/>
-      <Display value={bad}/>
+
+      <Statistics good = {good} neutral={neutral} bad = {bad}/>
+
+
          
     </div>
       
