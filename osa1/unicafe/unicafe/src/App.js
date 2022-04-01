19ad4e22d35086d01f2a7774932ee8e67cfa633a
stevari <villevartia@hotmail.com>
2022-04-01 15:24:27 +0300

    unicafe step 6, kaikki toimii, ei varoituksia consolissa

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -4,52 +4,64 @@ const Button = ({ handleClick, text }) => (
     {text}
   </button>
 )
-const Display = ({ value }) => <div>{value}</div>
-const StatisticLine = ({ text,value,text2 }) => <div>{text} {value} {text2} </div>
-const Statistics = ({good,neutral,bad}) => {
+//const Display = ({ value }) => <div>{value}</div>
+const StatisticLine = ({ text, value, text2 }) =>
+  <tr>
+    <td>{text} </td>
+    <td>
+      {value} {text2}
+    </td>
+
+  </tr>
+const Statistics = ({ good, neutral, bad }) => {
   /// ...
-  let all = good+neutral+bad;
+  let all = good + neutral + bad;
   let average = 0;
-  average = (good -1*bad)/all;
+  average = (good - 1 * bad) / all;
 
   let pospercentige = 0;
-  pospercentige = (good/all)*100;
-  if(all>0){ //show stats only when there has been atleast one user input
-    return(
-    
+  pospercentige = (good / all) * 100;
+  if (all > 0) { //show stats only when there has been atleast one user input
+    return (
+
       <div>
-        <StatisticLine text="good" value ={good} />
-        <StatisticLine text="neutral" value ={neutral} />
-        <StatisticLine text="bad" value ={bad} />
-        <StatisticLine text="all" value ={all} />
-        <StatisticLine text="average" value ={average} />
-        <StatisticLine text="positive" value ={pospercentige} text2="%" />
-        
-  
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={all} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={pospercentige} text2="%" />
+          </tbody>
+
+        </table>
+
       </div>
     )
-  }else{
-    return(
+  } else {
+    return (
       <div>
         <h4>No feedback given :(</h4>
       </div>
     )
   }
- 
+
 }
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
-  
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  
+
   const addGoodRev = () => setGood(good + 1)
   const addNeutralRev = () => setNeutral(neutral + 1)
   const addBadRev = () => setBad(bad + 1)
- 
+
 
   return (
     <div>
@@ -69,17 +81,17 @@ const App = () => {
       />
       <br></br>
 
-      <h2>Statistics:</h2> 
+      <h2>Statistics:</h2>
 
-      <br></br> 
-     
-      <Statistics good = {good} neutral={neutral} bad = {bad}/>
+      <br></br>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
 
 
-         
+
     </div>
-      
-  
+
+
   )
 }
 
